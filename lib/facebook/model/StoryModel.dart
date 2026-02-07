9b14a980813835e1ad1aecebf91142dd0aec3a21
omer1998 <omerfaris11@gmail.com>
2026-02-07 05:20:40 +0300

    create StoryModel class with profile and story image URLs and add example data

---
+++ lib/facebook/model/StoryModel.dart
@@ -0,0 +1,56 @@
+class StoryModel {
+  String profileImgUrl;
+  List<String> storiesImgUrl;
+
+  StoryModel({required this.profileImgUrl, required this.storiesImgUrl});
+
+  static List<StoryModel> example = [
+    StoryModel(
+      profileImgUrl: 'https://avatars.githubusercontent.com/u/39383435?v=4',
+      storiesImgUrl: [
+        'https://picsum.photos/id/1/1000/1000',
+        'https://picsum.photos/id/2/1000/1000',
+        'https://picsum.photos/id/3/1000/1000',
+      ],
+    ),
+    StoryModel(
+      profileImgUrl: 'https://avatars.githubusercontent.com/u/39383435?v=4',
+      storiesImgUrl: [
+        'https://picsum.photos/id/1/1000/1000',
+        'https://picsum.photos/id/2/1000/1000',
+        'https://picsum.photos/id/3/1000/1000',
+      ],
+    ),StoryModel(
+      profileImgUrl: 'https://avatars.githubusercontent.com/u/39383435?v=4',
+      storiesImgUrl: [
+        'https://picsum.photos/id/1/1000/1000',
+        'https://picsum.photos/id/2/1000/1000',
+        'https://picsum.photos/id/3/1000/1000',
+      ],
+    ),
+    StoryModel(
+      profileImgUrl: 'https://avatars.githubusercontent.com/u/39383435?v=4',
+      storiesImgUrl: [
+        'https://picsum.photos/id/1/1000/1000',
+        'https://picsum.photos/id/2/1000/1000',
+        'https://picsum.photos/id/3/1000/1000',
+      ],
+    ),
+    StoryModel(
+      profileImgUrl: 'https://avatars.githubusercontent.com/u/39383435?v=4',
+      storiesImgUrl: [
+        'https://picsum.photos/id/1/1000/1000',
+        'https://picsum.photos/id/2/1000/1000',
+        'https://picsum.photos/id/3/1000/1000',
+      ],
+    ),
+    StoryModel(
+      profileImgUrl: 'https://avatars.githubusercontent.com/u/39383435?v=4',
+      storiesImgUrl: [
+        'https://picsum.photos/id/1/1000/1000',
+        'https://picsum.photos/id/2/1000/1000',
+        'https://picsum.photos/id/3/1000/1000',
+      ],
+    ),
+  ];
+}
