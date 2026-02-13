0558846fe33a5f8a1827a6a415db593b9704da96
omer1998 <omerfaris11@gmail.com>
2026-02-14 00:43:34 +0300

    implement Facebook login screen with form validation and custom UI components

---
+++ lib/facebook/loginScreen.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basic_training/facebook/core/ColorManager.dart';
+import 'package:flutter_basic_training/facebook/customWidgets/inputTextField.dart';
+import 'package:flutter_basic_training/facebook/customWidgets/primaryButton.dart';
+import 'package:flutter_basic_training/generated/assets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(height: 70),
+                  SvgPicture.asset(
+                    Assets.iconsIcFacebookLogo,
+                    width: 100,
+                    height: 100,
+                  ),
+                  SizedBox(height: 60),
+                  FaceBookInputField(
+                    hint: "Mobile Number or Email Address",
+                    obscureText: false,
+                    validator: (text) {
+                      if (text == null || text.isEmpty) {
+                        return "this field is required";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  SizedBox(height: 24),
+                  FaceBookInputField(
+                    hint: "Password",
+                    obscureText: true,
+                    validator: (text) {
+                      print("===> text");
+                      if (text == null || text.isEmpty) {
+                        return "this field is required";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  SizedBox(height: 40),
+
+                  PrimaryButton(
+                    text: "Login",
+                    filledColor: ColorManager.blue,
+                    textColor: Colors.white,
+                    onTap: () {
+                      if (_formKey.currentState!.validate()) {
+                        print("valid input");
+                      }
+                    },
+                  ),
+
+                  ///SizedBox(height: 24),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Forgotten Password ?",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              children: [
+                PrimaryButton(
+                  text: "Create Account",
+                  filledColor: Colors.white,
+                  textColor: ColorManager.blue,
+                  onTap: () {},
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      Assets.iconsIcMetaLogo,
+                      width: 24,
+                      height: 24,
+                    ),
+                    Text(
+                      "Meta",
+                      style: TextStyle(color: ColorManager.blue, fontSize: 24),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
